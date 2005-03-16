33f60e629b6097cc8ddc8caf1f0c0501e8177a96
Sebastian Bauer <mail@sebastianbauer.info>
2005-03-16 13:10:04 +0000

    Added custom SDI_iface.h to abstract the interface handling of OS4.

---
+++ soliton/common/SDI_iface.h
@@ -0,0 +1,16 @@
+#ifndef SDI_IFACE_H
+#define SDI_IFACE_H
+
+#ifdef __amigaos4__
+#define EXEC_INTERFACE_DECLARE(x) x
+#define EXEC_INTERFACE_GET_MAIN(interface,libbase) (interface = (void*)GetInterface((struct Library*)(libbase),"main",1,NULL))
+#define EXEC_INTERFACE_DROP(interface) DropInterface((struct Interface*)interface)
+#define EXEC_INTERFACE_ASSIGN(a,b) (a = (b))
+#else
+#define EXEC_INTERFACE_DECLARE(x)
+#define EXEC_INTERFACE_GET_MAIN(interface,libbase) 1
+#define EXEC_INTERFACE_DROP(interface)
+#define EXEC_INTERFACE_ASSIGN(a,b)
+#endif
+
+#endif
